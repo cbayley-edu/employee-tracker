2a018055d76bb9d11b8e346167cfef58df34c977
christina-edu <christina-edu@users.noreply.github.com>
2020-08-10 00:05:32 -0400

    updated sql queries

--- assets/sql/select.sql
+++ assets/sql/select.sql
@@ -1,20 +1,39 @@
 USE employee_tracker_db;
 
-SELECT * FROM department;
+SELECT  *
+FROM    department;
 
-SELECT * FROM role;
+SELECT  *
+FROM    role;
 
-SELECT * FROM employee;
+SELECT  *
+FROM    employee;
 
-SELECT *
-FROM employee e
+SELECT  *
+FROM    employee e
 LEFT JOIN role r ON r.id = e.role_id
 LEFT JOIN department d on d.id = r.department_id
 LEFT JOIN employee m on m.id = e.manager_id;
 
-
-SELECT e.id, e.first_name, e.last_name, r.title, d.name department, ROUND(r.salary, 0) salary, CONCAT(m.first_name, " ", m.last_name) manager
-FROM employee e
+-- ALL EMPLOYEES
+SELECT  e.id, e.first_name, e.last_name, r.title, d.name department, ROUND(r.salary, 0) salary, CONCAT(m.first_name, " ", m.last_name) manager
+FROM    employee e
 LEFT JOIN role r ON r.id = e.role_id
 LEFT JOIN department d on d.id = r.department_id
 LEFT JOIN employee m on m.id = e.manager_id;
+
+-- ACTIVE EMPLOYEES
+SELECT  e.id, e.active, e.first_name, e.last_name, r.title, d.name department, ROUND(r.salary, 0) salary, CONCAT(m.first_name, " ", m.last_name) manager
+FROM    employee e
+LEFT JOIN role r ON r.id = e.role_id
+LEFT JOIN department d on d.id = r.department_id
+LEFT JOIN employee m on m.id = e.manager_id
+WHERE   e.active = true;
+
+-- INACTIVE EMPLOYEES
+SELECT  e.id, e.active, e.status, e.first_name, e.last_name, r.title, d.name department, ROUND(r.salary, 0) salary, CONCAT(m.first_name, " ", m.last_name) manager
+FROM    employee e
+LEFT JOIN role r ON r.id = e.role_id
+LEFT JOIN department d on d.id = r.department_id
+LEFT JOIN employee m on m.id = e.manager_id
+WHERE   e.active = false;
