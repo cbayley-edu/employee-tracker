0061ede48a409f6362aecfc879ea4ab860b004de
christina-edu <christina-edu@users.noreply.github.com>
2020-08-09 23:59:38 -0400

    added sql files

--- assets/sql/seed.sql
+++ assets/sql/seed.sql
@@ -18,46 +18,49 @@ INSERT INTO `employee_tracker_db`.`role` (`title`, `salary`, `department_id`)
 VALUES ("Accountant", 125000, 2);         -- id 1
 
 INSERT INTO `employee_tracker_db`.`role` (`title`, `salary`, `department_id`)
-VALUES ("Lawyer", 190000, 3);             -- id 2
+VALUES ("Account Manager", 75000, 2);     -- id 2
 
 INSERT INTO `employee_tracker_db`.`role` (`title`, `salary`, `department_id`)
-VALUES ("Lead Engineer", 150000, 1);      -- id 3
+VALUES ("Lawyer", 190000, 3);             -- id 3
 
 INSERT INTO `employee_tracker_db`.`role` (`title`, `salary`, `department_id`)
-VALUES ("Legal Team Lead", 250000, 2);    -- id 4
+VALUES ("Lead Engineer", 150000, 1);      -- id 4
 
 INSERT INTO `employee_tracker_db`.`role` (`title`, `salary`, `department_id`)
-VALUES ("Sales Lead", 100000, 4);         -- id 5
+VALUES ("Legal Team Lead", 250000, 2);    -- id 5
 
 INSERT INTO `employee_tracker_db`.`role` (`title`, `salary`, `department_id`)
-VALUES ("Salesperson", 80000, 4);         -- id 6
+VALUES ("Sales Lead", 100000, 4);         -- id 6
 
 INSERT INTO `employee_tracker_db`.`role` (`title`, `salary`, `department_id`)
-VALUES ("Software Engineer", 120000, 1);  -- id 7
+VALUES ("Salesperson", 80000, 4);         -- id 7
+
+INSERT INTO `employee_tracker_db`.`role` (`title`, `salary`, `department_id`)
+VALUES ("Software Engineer", 120000, 1);  -- id 8
 
 -- Insert into employee table
 INSERT INTO `employee_tracker_db`.`employee` (`first_name`, `last_name`, `role_id`, `manager_id`)
-VALUES ("John", "Doe", 5, 3);               -- id 1
+VALUES ("John", "Doe", 6, 3);               -- id 1
 
 INSERT INTO `employee_tracker_db`.`employee` (`first_name`, `last_name`, `role_id`, `manager_id`)
-VALUES ("Mike", "Chan", 6, 1);              -- id 2
+VALUES ("Mike", "Chan", 7, 1);              -- id 2
 
 INSERT INTO `employee_tracker_db`.`employee` (`first_name`, `last_name`, `role_id`)
-VALUES ("Ashley", "Rodriguez", 3);          -- id 3
+VALUES ("Ashley", "Rodriguez", 4);          -- id 3
 
 INSERT INTO `employee_tracker_db`.`employee` (`first_name`, `last_name`, `role_id`, `manager_id`)
-VALUES ("Kevin", "Tupik", 7, 1);            -- id 4
+VALUES ("Kevin", "Tupik", 8, 1);            -- id 4
 
 INSERT INTO `employee_tracker_db`.`employee` (`first_name`, `last_name`, `role_id`)
 VALUES ("Malia", "Brown", 1);               -- id 5
 
 INSERT INTO `employee_tracker_db`.`employee` (`first_name`, `last_name`, `role_id`)
-VALUES ("Sarah", "Lourd", 4);               -- id 6
+VALUES ("Sarah", "Lourd", 5);               -- id 6
 
 INSERT INTO `employee_tracker_db`.`employee` (`first_name`, `last_name`, `role_id`, `manager_id`)
-VALUES ("Tom", "Allen", 2, 6);              -- id 7
+VALUES ("Tom", "Allen", 3, 6);              -- id 7
 
 INSERT INTO `employee_tracker_db`.`employee` (`first_name`, `last_name`, `role_id`, `manager_id`)
-VALUES ("Christian", "Eckenrode", 3, 2);    -- id 8
+VALUES ("Christian", "Eckenrode", 4, 2);    -- id 8
 
 
--- assets/sql/select.sql
+++ assets/sql/select.sql
@@ -13,7 +13,7 @@ LEFT JOIN department d on d.id = r.department_id
 LEFT JOIN employee m on m.id = e.manager_id;
 
 
-SELECT e.id, e.first_name, e.last_name, r.title, d.name department, r.salary, CONCAT(m.first_name, " ", m.last_name) manager
+SELECT e.id, e.first_name, e.last_name, r.title, d.name department, ROUND(r.salary, 0) salary, CONCAT(m.first_name, " ", m.last_name) manager
 FROM employee e
 LEFT JOIN role r ON r.id = e.role_id
 LEFT JOIN department d on d.id = r.department_id
